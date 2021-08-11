311a77eb811d53535783390e52ea9b952d747c57
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2021-08-10 20:11:58 -0700

    update

--- run.sh
+++ run.sh
@@ -1,11 +1,16 @@
 docker-compose down
-docker rmi -f $(docker images -f "dangling=true" -q)
+
 pushd ../api
 ./mvnw clean package
 docker build -t yw/whatsinme-api .
 popd
+
 pushd ../app
 npm run build-local
 docker build -t yw/whatsinme-app-dev -f Dockerfile-dev .
 popd
+
 docker-compose -f docker-compose-dev.yml up -d
+docker rmi -f $(docker images -f "dangling=true" -q)
+
+chromium http://localhost:8080
